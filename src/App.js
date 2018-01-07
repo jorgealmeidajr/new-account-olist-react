5db4d34158afc624dd06775814e1923812a5ad53
Jorge Almeida Junior <jorgealmeidajunior@gmail.com>
2018-01-07 17:00:59 -0200

    feat: Initial new account layout added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Button } from 'reactstrap'
+import { Form, FormGroup, Label, Input, Button, Progress } from 'reactstrap'
 
 import logo from './logo.svg'
 import './App.css'
@@ -13,12 +13,43 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <Button color="danger">Danger!</Button>
-        </p>
+        <div className="App-intro">
+
+          <Form>
+            <FormGroup>
+              <Label for="user_name">Nome Completo</Label>
+              <Input type="text" name="user_name" id="user_name" placeholder="" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="user_email">Email</Label>
+              <Input type="email" name="user_email" id="user_email" placeholder="" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="user_password">Senha</Label>
+              <Input type="password" name="user_password" id="user_password" placeholder="" />
+            </FormGroup>
+
+            <div>
+              <Progress value={50} color='success' />
+              <div>
+                Pelo menos 6 caracteres<br />
+                Pelo menos 1 letra maiúscula<br />
+                Pelo menos 1 número
+              </div>
+            </div>
+
+            <FormGroup>
+              <Label for="user_password_conf">Confirme a sua Senha</Label>
+              <Input type="password" name="user_password_conf" id="user_password_conf" placeholder="" />
+            </FormGroup>
+
+            <Button size="lg" block style={{backgroundColor: '#17D499'}} onClick={(e) => e.preventDefault()}>Criar conta</Button>
+          </Form>
+
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
