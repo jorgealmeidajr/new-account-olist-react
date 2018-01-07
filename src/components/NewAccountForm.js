6a64dca0d08c94087137a5eb8a5e03339053380d
Jorge Almeida Junior <jorgealmeidajunior@gmail.com>
2018-01-07 18:20:58 -0200

    feat: Olist logo added. NewAccountForm css added.

--- src/components/NewAccountForm.js
+++ src/components/NewAccountForm.js
@@ -3,46 +3,56 @@ import React, { Component } from 'react'
 
 import { Form, FormGroup, Label, Input, Button, Progress, Badge } from 'reactstrap'
 
+import './NewAccountForm.css'
+
 export default class NewAccountForm extends Component {
 
   render() {
     return (
-      <Form>
-        <FormGroup>
-          <Label for="name">Nome Completo</Label>
-          <Input type="text" name="name" id="name" placeholder="" valid />
-        </FormGroup>
-        <FormGroup>
-          <Label for="user_email">Email</Label>
-          <Input type="email" name="user_email" id="user_email" placeholder="" valid={false} />
-        </FormGroup>
-        <FormGroup>
-          <Label for="user_password">Senha</Label>
-          <Input type="password" name="user_password" id="user_password" placeholder="" />
-        </FormGroup>
-
-        <div>
-          <Progress value={50} color='success' />
-          <div>
-            <Badge color="secondary" style={{fontSize: 16, width: 400, textAlign: 'left'}}>Pelo menos 6 caracteres</Badge><br />
-            <Badge color="danger" style={{fontSize: 16, width: 400, textAlign: 'left'}}>Pelo menos 1 letra maiúscula</Badge><br />
-            <Badge color="success" style={{fontSize: 16, width: 400, textAlign: 'left'}}>Pelo menos 1 número</Badge><br />
-          </div>
+      <div className='newAccountContainer'>
+        <div className='newAccountHeader'>
+          <img src='olist.png' className="newAccountLogo" alt="logo" />
+          <h4>Crie sua conta</h4>
         </div>
 
-        <FormGroup>
-          <Label for="user_password_conf">Confirme a sua Senha</Label>
-          <Input type="password" name="user_password_conf" id="user_password_conf" placeholder="" />
-        </FormGroup>
+        <Form className='newAccountForm'>
+          <FormGroup>
+            <Label for="name">Nome Completo</Label>
+            <Input type="text" name="name" id="name" placeholder="" valid />
+          </FormGroup>
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input type="email" name="email" id="email" placeholder="" valid={false} />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Senha</Label>
+            <Input type="password" name="password" id="password" placeholder="" />
+          </FormGroup>
 
-        <Button 
-          size="lg" 
-          block 
-          style={{backgroundColor: '#17D499'}} 
-          active={false}
-          disabled={true}
-          onClick={(e) => e.preventDefault()}>Criar conta</Button>
-      </Form>
+          <div>
+            <Progress value={(100 / 3) * 2} color='success' />
+            <div>
+              <Badge color="secondary" className='newAccountBadge'>Pelo menos 6 caracteres</Badge><br />
+              <Badge color="danger" className='newAccountBadge'>Pelo menos 1 letra maiúscula</Badge><br />
+              <Badge color="success" className='newAccountBadge'>Pelo menos 1 número</Badge>
+            </div>
+            <br />
+          </div>
+
+          <FormGroup>
+            <Label for="passwordConfirmation">Confirme a sua Senha</Label>
+            <Input type="password" name="passwordConfirmation" id="passwordConfirmation" placeholder="" />
+          </FormGroup>
+
+          <Button 
+            size="lg" 
+            block 
+            style={{backgroundColor: '#17D499'}} 
+            active={false}
+            disabled={true}
+            onClick={(e) => e.preventDefault()}>Criar conta</Button>
+        </Form>
+      </div>
     )
   }
 
